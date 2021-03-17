441845d71f3cf133a17fc5d6eea9bab31dc77d56
Nicolas Garcia Lira <nico17.ngl@gmail.com>
2021-03-17 00:42:09 -0600

    Initial commit

---
+++ js/lightbox.js
@@ -0,0 +1,19 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light')
+const closeLight = document.querySelector('.close')
+
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click',()=>{
+        aparecerImagen(imagen.getAttribute('src'));
+    })
+});
+
+contenedorLight.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight){
+        contenedorLight.classList.toggle('show')
+        imagenLight.classList.toggle('showImage')
+        todo.style.opacity = '1';
+    }
+})
